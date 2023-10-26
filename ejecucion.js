45b4141c3fab81312280235478d130fb064ff35c
Aruroo <jonathan.arturo.preciado@ciencias.unam.mx>
2023-10-25 21:00:48 -0600

    refactorizacion + turnos

--- ejecucion.js
+++ ejecucion.js
@@ -1,5 +1,6 @@
-//false = esta libre, true = está ocupada
-const gato = [false, false, false, false, false, false, false, false, false];
+//0 = esta libre, 1 = está ocupado por x, 2 = está ocupado por o
+const gato = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let turno = 1;
 
 //recuperamos todas las celdas
 let c0 = document.querySelector('#celda0');
@@ -12,93 +13,31 @@ let c6 = document.querySelector('#celda6');
 let c7 = document.querySelector('#celda7');
 let c8 = document.querySelector('#celda8');
 
+function clickCelda(celda, index) {
+    if (gato[index] == 0) {
+        console.log(`Se hizo clic en ${celda.id}`);
+        gato[index] = turno;
+        if (turno == 1) {
+            celda.textContent = 'X';
+            turno = 2;
+        } else {
+            celda.textContent = 'O';
+            turno = 1;
+        }
+    } else {
+        console.log(`${celda.id} ya está ocupada`);
+        document.querySelector('#error').textContent = 'Esa celda ya está ocupada!!';
+    }
+}
+
 // Agregar un eventListener a cada celda
-c0.addEventListener('click', () => {
-    if (gato[0] == false) {
-        console.log('Se hizo clic en c0');
-        gato[0] = true ;
-    } else {
-        console.log('c0 ya está ocupada');
-        document.querySelector('#error').textContent = 'Esa celda ya está ocupada!!';
-    }
-});
+c0.addEventListener('click', () => clickCelda(c0, 0));
+c1.addEventListener('click', () => clickCelda(c1, 1));
+c2.addEventListener('click', () => clickCelda(c2, 2));
+c3.addEventListener('click', () => clickCelda(c3, 3));
+c4.addEventListener('click', () => clickCelda(c4, 4));
+c5.addEventListener('click', () => clickCelda(c5, 5));
+c6.addEventListener('click', () => clickCelda(c6, 6));
+c7.addEventListener('click', () => clickCelda(c7, 7));
+c8.addEventListener('click', () => clickCelda(c8, 8));
 
-c1.addEventListener('click', () => {
-    if (gato[1] == false) {
-        console.log('Se hizo clic en c1');
-        gato[1] = true ;
-    } else {
-        console.log('c1 ya está ocupada');
-        document.querySelector('#error').textContent = 'Esa celda ya está ocupada!!';
-    }
-});
-
-c2.addEventListener('click', () => {
-    if (gato[2] == false) {
-        console.log('Se hizo clic en c2');
-        gato[2] = true ;
-    } else {
-        console.log('c2 ya está ocupada');
-        document.querySelector('#error').textContent = 'Esa celda ya está ocupada!!';
-    }
-});
-
-c3.addEventListener('click', () => {
-    if (gato[3] == false) {
-        console.log('Se hizo clic en c3');
-        gato[3] = true ;
-    } else {
-        console.log('c3 ya está ocupada');
-        document.querySelector('#error').textContent = 'Esa celda ya está ocupada!!';
-    }
-});
-
-c4.addEventListener('click', () => {
-    if (gato[4] == false) {
-        console.log('Se hizo clic en c4');
-        gato[4] = true ;
-    } else {
-        console.log('c4 ya está ocupada');
-        document.querySelector('#error').textContent = 'Esa celda ya está ocupada!!';
-    }
-});
-
-c5.addEventListener('click', () => {
-    if (gato[5] == false) {
-        console.log('Se hizo clic en c5');
-        gato[5] = true ;
-    } else {
-        console.log('c5 ya está ocupada');
-        document.querySelector('#error').textContent = 'Esa celda ya está ocupada!!';
-    }
-});
-
-c6.addEventListener('click', () => {
-    if (gato[6] == false) {
-        console.log('Se hizo clic en c6');
-        gato[6] = true ;
-    } else {
-        console.log('c6 ya está ocupada');
-        document.querySelector('#error').textContent = 'Esa celda ya está ocupada!!';
-    }
-});
-
-c7.addEventListener('click', () => {
-    if (gato[7] == false) {
-        console.log('Se hizo clic en c7');
-        gato[7] = true ;
-    } else {
-        console.log('c7 ya está ocupada');
-        document.querySelector('#error').textContent = 'Esa celda ya está ocupada!!';
-    }
-});
-
-c8.addEventListener('click', () => {
-    if (gato[8] == false) {
-        console.log('Se hizo clic en c8');
-        gato[8] = true ;
-    } else {
-        console.log('c8 ya está ocupada');
-        document.querySelector('#error').textContent = 'Esa celda ya está ocupada!!';
-    }
-});
